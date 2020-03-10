0231ac2376f52f9a0afeb35dad6283e7e7f10ae6
GS Sidhu <sid8247@gmail.com>
2020-03-09 22:40:55 -0700

    testing components

---
+++ app/src/main/java/com/example/seefood/dataLoadingActivity.java
@@ -0,0 +1,16 @@
+package com.example.seefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class dataLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_loading);
+    }
+}
--- app/src/main/java/com/example/seefood/userProfileActivity.java
+++ app/src/main/java/com/example/seefood/userProfileActivity.java
@@ -2,7 +2,9 @@ package com.example.seefood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class userProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class userProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    public void postData(View view){
+        Intent intent = new Intent(this, dataLoadingActivity.class);
+        //use put extra to pass things to the activity
+        startActivity(intent);
     }
 }
