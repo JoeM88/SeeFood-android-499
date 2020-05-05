5194b3a25b9b9d1bd06e930a68e8f60d60ae316e
GS Sidhu <gsidhu@csumb.edu>
2020-05-05 13:11:19 -0700

    Patched onStart() in favorites fragment

--- app/src/main/java/com/example/seefood/favorites/FragmentFavorite.java
+++ app/src/main/java/com/example/seefood/favorites/FragmentFavorite.java
@@ -3,6 +3,7 @@ package com.example.seefood.favorites;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,54 +11,34 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.example.seefood.FragmentProfile;
 import com.example.seefood.MainActivity;
 import com.example.seefood.R;
-import com.example.seefood.displayProfiles.dispCustomerProfile;
-import com.example.seefood.displayProfiles.displayRestaurantProfile;
 import com.example.seefood.login.SignUpActivity;
 import com.example.seefood.models.CustomerModel;
 import com.example.seefood.models.RestaurantModel;
-import com.example.seefood.profileSetup.createProfileActivity;
 import com.example.seefood.restaurantDetails.FragmentRestaurantDetails;
 import com.example.seefood.restaurantList.RecyclerViewAdapter;
-import com.example.seefood.restaurantList.Restaurant;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
 
 public class FragmentFavorite extends Fragment {
     private View v;
     private RecyclerView mRecyclerView;
     private RecyclerViewAdapter mAdapter;
     private Context mContext;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore db;
     private FirebaseUser currentUser;
     private DocumentReference customerRef;
@@ -72,8 +53,9 @@ public class FragmentFavorite extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser().getUid() != null) {
+        //mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
             this.uid = mAuth.getCurrentUser().getUid();
         } else {
             goSignUp(null);
@@ -83,7 +65,11 @@ public class FragmentFavorite extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        Handler handler = new Handler();
+
         v = inflater.inflate(R.layout.favorite_fragment, container, false);
+        onStart();
         mContext = getContext();
 
         mRecyclerView = v.findViewById(R.id.favorite_list_fragment);
